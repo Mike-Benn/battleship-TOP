a11fff5ec75b76b8f7f1b7461ccaf26b006b4702
Mike-Benn <mbenn49@wgu.edu>
2024-05-20 04:08:41 -0400

    Add tests for placeShip,receiveAttack,isGameOver

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,7 +1,8 @@
 import { GameBoard } from "../src/gameboard";
 import { Ship } from "../src/ship";
 import { GameSpace } from "../src/gamespace";
-import { TIME_UNITS_LEN } from "cron/dist/constants";
+
+//getBoard()
 
 test('Returns the game board array.' , () => {
     let gameboard = GameBoard();
@@ -21,13 +22,28 @@ test('Returns the game board array.' , () => {
 
 });
 
-test('Returns a map of all current ship locations.' , () => {
-    let gameboard = GameBoard();
-    let testMap = new Map();
+//getShipCoordinates()
 
-    expect(gameboard.getShipCoordinates()).toStrictEqual(testMap);
+test('Returns an object of all current ship locations.' , () => {
+    let gameboard = GameBoard();
+    let testObject= {
+        0 : [],
+        1 : [],
+        2 : [],
+        3 : [],
+        4 : [],
+        5 : [],
+        6 : [],
+        7 : [],
+        8 : [],
+        9 : [],
+    };
+
+    expect(gameboard.getShipCoordinates()).toStrictEqual(testObject);
 
 })
+
+//getInactiveSquares()
 
 test('Returns an object representing already played moves.' , () => {
     let gameboard = GameBoard();
@@ -37,6 +53,8 @@ test('Returns an object representing already played moves.' , () => {
     expect(gameboard.getInactiveSquares()[0][0]).toBe("test");
     expect(gameboard.getInactiveSquares()[9][0]).toBe("test");
 })
+
+//resetInactiveSquares()
 
 test('Reset inactive squares object to original state.' , () => {
     let gameboard = GameBoard();
@@ -48,6 +66,8 @@ test('Reset inactive squares object to original state.' , () => {
     gameboard.resetInactiveSquares();
     expect(gameboard.getInactiveSquares()[0]).toStrictEqual([]);
 })
+
+//resetBoard()
 
 test('Resets the game board array.' , () => {
     let gameboard = GameBoard();
@@ -69,14 +89,18 @@ test('Resets the game board array.' , () => {
 
 });
 
+//resetCoordinates()
+
 test('Resets the list of ship coordinates.' , () => {
     let gameboard = GameBoard();
-    gameboard.getShipCoordinates().set('a' , 1);
+    gameboard.getShipCoordinates()['a'] = 1;
 
-    expect(gameboard.getShipCoordinates().has('a')).toBe(true);
+    expect(gameboard.getShipCoordinates().hasOwnProperty('a')).toBe(true);
     gameboard.resetCoordinates();
-    expect(gameboard.getShipCoordinates().has('a')).toBe(false);
+    expect(gameboard.getShipCoordinates().hasOwnProperty('a')).toBe(false);
 })
+
+//generateBoard()
 
 test('Generates a game board array of game space objects.' , () => {
     let gameboard = GameBoard();
@@ -85,10 +109,107 @@ test('Generates a game board array of game space objects.' , () => {
     expect(gameboard.getBoard()[0][0].isSpaceActive()).toBe(true);
 })
 
+//getSpaceAt()
+
 test('Gets game space at coordinates.' , () => {
     let gameboard = GameBoard();
     gameboard.generateBoard();
 
     expect(gameboard.getSpaceAt(1 , 1).getLocation()).toStrictEqual([1 , 1]);
 });
+
+//placeShip()
+
+test('Places horizontal ship on the board and updates appropriate variables' , () => {
+    let gameboard = GameBoard();
+    gameboard.generateBoard();
+
+    let ship = Ship(5 , "carrier");
+    gameboard.placeShip(ship , 1 , 1);
+
+    expect(gameboard.getSpaceAt(1 , 1).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(1 , 2).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(1 , 3).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(1 , 4).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(1 , 5).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(1 , 6).getOccupiedBy()).toBe(null);
+    expect(gameboard.getSpaceAt(2 , 1).getOccupiedBy()).toBe(null);
+    
+
+})
+
+test('Places vertical ship on the board and updates appropriate variables' , () => {
+    let gameboard = GameBoard();
+    gameboard.generateBoard();
+
+    let ship = Ship(5 , "carrier");
+    ship.toggleOrientation();
+    gameboard.placeShip(ship , 1 , 1);
+
+    expect(gameboard.getSpaceAt(1 , 1).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(2 , 1).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(3 , 1).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(4 , 1).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(5 , 1).getOccupiedBy()).toBe(ship);
+    expect(gameboard.getSpaceAt(6 , 1).getOccupiedBy()).toBe(null);
+    expect(gameboard.getSpaceAt(1 , 2).getOccupiedBy()).toBe(null);
+    
+
+})
+//receiveAttack()
+
+test('Receives attack from player and it\'s a hit.' , () => {
+    let gameboard = GameBoard();
+    gameboard.generateBoard();
+
+    let ship = Ship(1 , "test");
+    gameboard.placeShip(ship , 1 , 1);
+
+    expect(gameboard.getSunkenShips().length).toBe(0);
+    gameboard.receiveAttack(1 , 1);
+    let inactiveSquares = gameboard.getInactiveSquares();
+
+    
+    expect(inactiveSquares[1][0]).toBe(1);
+    expect(inactiveSquares[1][1]).toBe(undefined);
+    expect(inactiveSquares[2][0]).toBe(undefined);
+    expect(ship.getHealth()).toBe(0);
+    expect(gameboard.getSunkenShips().length).toBe(1);
+
+    
+    
+
+})
+
+test('Receives attack from player and it\'s a miss.' , () => {
+    let gameboard = GameBoard();
+    gameboard.generateBoard();
+
+    let ship = Ship(5 , "test");
+    gameboard.placeShip(ship , 1 , 1);
+
+    gameboard.receiveAttack(2 , 1);
+    let inactiveSquares = gameboard.getInactiveSquares();
+
+    expect(inactiveSquares[2][0]).toBe(1);
+    expect(inactiveSquares[2][1]).toBe(undefined);
+    expect(inactiveSquares[1][0]).toBe(undefined);
+    expect(ship.getHealth()).toBe(5);
+    expect(gameboard.getSunkenShips().length).toBe(0);
+})
+
+//isGameOver()
+
+test('Returns true or false whether or not all ships are sunk.' , () => {
+    let gameboard = GameBoard();
+
+    expect(gameboard.isGameOver()).toBe(false);
+    gameboard.getSunkenShips().push(1);
+    gameboard.getSunkenShips().push(1);
+    gameboard.getSunkenShips().push(1);
+    expect(gameboard.isGameOver()).toBe(false);
+    gameboard.getSunkenShips().push(1);
+    expect(gameboard.isGameOver()).toBe(true);
+
+})
 
