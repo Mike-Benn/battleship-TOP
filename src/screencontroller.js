5e0580159851f3b1ba4b969b3fb28beb0bc12a87
Mike-Benn <mbenn49@wgu.edu>
2024-05-24 11:19:08 -0400

    Create screencontroller factory fx

---
+++ src/screencontroller.js
@@ -0,0 +1,17 @@
+import { GameController } from "./gamecontroller";
+
+function ScreenController() {
+    let game = null;
+
+
+
+    const run = () => {
+        game = GameController();
+    }
+
+    const updateMainScreen = () => {
+        let gridContainer = document.createElement('section');
+        
+    }
+
+}
