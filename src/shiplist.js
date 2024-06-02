8e777702a228aaa0ab8e23fedcb23f5d614a19a7
Mike-Benn <mbenn49@wgu.edu>
2024-06-02 00:07:38 -0400

    Reverse ship order in list and amend removeShip

--- src/shiplist.js
+++ src/shiplist.js
@@ -2,13 +2,13 @@ import { Ship } from "./ship";
 export { ShipList };
 
 function ShipList() {
-    let ships = [Ship(2 , "patrol") , Ship(3 , "submarine") , Ship(4 , "battleship") , Ship(5 , "carrier")];
+    let ships = [Ship(5 , "carrier") , Ship(4 , "battleship") , Ship(3 , "submarine") , Ship(2 , "patrol")];
 
     const getShips = () => ships;
 
     const removeShip = () => {
         if (ships.length > 0) {
-            ships.pop();
+            ships.shift();
         }
     }
 
