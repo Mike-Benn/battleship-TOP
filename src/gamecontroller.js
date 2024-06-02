f4c8f064d46e8385e2cb603e6a2f9fdb15481944
Mike-Benn <mbenn49@wgu.edu>
2024-06-02 00:46:26 -0400

    Remove stopGame method

--- src/gamecontroller.js
+++ src/gamecontroller.js
@@ -2,47 +2,42 @@ import { Player } from "./player";
 import { GameBoard } from "./gameboard";
 export { GameController };
 
-
-
 function GameController() {
-    let playerOne = Player("Player" , "Human");
-    let playerTwo = Player("CPU" , "Computer");
-    let pregameBoard = GameBoard();
-    let currentTurn = 0;
-    let phase = "pregame";
+  let playerOne = Player("Player", "Human");
+  let playerTwo = Player("CPU", "Computer");
+  let pregameBoard = GameBoard();
+  let currentTurn = 0;
+  let phase = "pregame";
 
+  const getPlayerOne = () => playerOne;
 
-    const getPlayerOne = () => playerOne;
+  const getPlayerTwo = () => playerTwo;
 
-    const getPlayerTwo = () => playerTwo;
-    
-    const getPregameBoard = () => pregameBoard;
+  const getPregameBoard = () => pregameBoard;
 
-    const getTurn = () => currentTurn;
+  const getTurn = () => currentTurn;
 
-    const getPhase = () => phase;
+  const getPhase = () => phase;
 
-    const switchTurn = () => {
-        if (currentTurn === 0) {
-            currentTurn = 1;
-        } else {
-            currentTurn = 0;
-        }
+  const switchTurn = () => {
+    if (currentTurn === 0) {
+      currentTurn = 1;
+    } else {
+      currentTurn = 0;
     }
+  };
 
-    const startGame = () => {
-        phase = "live";
-    }
+  const startGame = () => {
+    phase = "live";
+  };
 
-    return {
-        getPlayerOne,
-        getPlayerTwo,
-        getPregameBoard,
-        getTurn,
-        getPhase,
-        switchTurn,
-        startGame
-    }
-
-
+  return {
+    getPlayerOne,
+    getPlayerTwo,
+    getPregameBoard,
+    getTurn,
+    getPhase,
+    switchTurn,
+    startGame,
+  };
 }
