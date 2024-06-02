b564093d9f9ba073178131bbec109e354d358759
Mike-Benn <mbenn49@wgu.edu>
2024-06-02 03:12:04 -0400

    Remove unused methods and variables

--- src/gamespace.js
+++ src/gamespace.js
@@ -1,31 +1,14 @@
 export { GameSpace };
 
-function GameSpace(row, col) {
-  let location = [row, col];
+function GameSpace() {
   let occupiedBy = null;
-  let active = true;
-
-  const getLocation = () => location;
 
   const getOccupiedBy = () => occupiedBy;
 
-  const isSpaceActive = () => active;
-
-  const setLocation = (row, col) => {
-    location = [row, col];
-  };
-
   const setOccupiedBy = (ship) => (occupiedBy = ship);
 
-  const toggleSpaceActive = () => {
-    active = false;
-  };
   return {
-    getLocation,
     getOccupiedBy,
-    isSpaceActive,
-    setLocation,
     setOccupiedBy,
-    toggleSpaceActive,
   };
 }
